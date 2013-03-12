1f6f194c2d6995b7a3d4b1cf9447870ead4363de
ordepdev <pedrotavares.dev@gmail.com>
2013-03-12 22:49:44 +0000

    changes on skill.rb

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -14,4 +14,21 @@ class Skill < ActiveRecord::Base
   	def self.human_attribute_name(attr, options={})
   		HUMANIZED_ATTRIBUTES[attr.to_sym] || super
   	end
+
+  	def top5 
+  		Skill.count(:group => 'name', :limit => 5)
+  		# skills = ...
+  		# skills.each {|skill| 
+  		# 	puts 'Skill => ' + skill[0] 
+  		# 	puts 'Count => ' + skill[1]
+  		# }
+	end
+
+	def users_with_same_skill(skill)
+		Skill.where("name = #{skill}").map(&:user_id)
+		# users = ...
+		# users.each {|user|}
+		# 	Profile.find_by_user_id(user.id)
+		# ...
+	end
 end
